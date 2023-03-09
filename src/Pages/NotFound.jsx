d2a0c4649b51fa40290c962ef910ff94aa06189f
Victor Patrick <victorbernardpatrick@gmail.com>
2023-03-09 17:48:05 +0100

    fixed phone comment

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import Header from "./Header";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <div>
-      {/* <Header /> */}
+     
       <div className="not-found">
       
         <div>
         
           <h1>404 - Page Not Found</h1>
           <p>Sorry, the page you are looking for does not exist.</p>
+          <Link to="/" className="link-home">Home</Link>
         </div>
       </div>
     </div>
